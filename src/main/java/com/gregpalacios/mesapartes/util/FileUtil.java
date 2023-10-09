70621ab160554e3d03eca633f45b3d0618511886
Gregory Palacios <gregpalacios26@gmail.com>
2023-10-09 09:14:15 -0500

    Cambios en la clase FileUtil

--- src/main/java/com/gregpalacios/mesapartes/util/FileUtil.java
+++ src/main/java/com/gregpalacios/mesapartes/util/FileUtil.java
@@ -4,9 +4,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.sql.Timestamp;
 import java.util.Base64;
 
@@ -17,34 +14,30 @@ import com.gregpalacios.mesapartes.dto.FileDTO;
 @Component
 public class FileUtil {
 
-	public static String createFile(byte[] archivoByte, FileDTO fileDTO) throws IOException {
-
-		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-		String fileNameUniq = "file-" + timestamp.getTime() + "." + fileDTO.getFileFormat();
-		String filePath = fileDTO.getFilePath() + fileNameUniq;
-
-		Path path = Paths.get(filePath);
-		Files.createFile(path);
-
-		try (OutputStream out = new FileOutputStream(filePath);) {
-			out.write(archivoByte);
-			return fileNameUniq;
-		}
-	}
-
-	public static String createFileFromBase64(FileDTO fileDTO) throws IOException {
-
-		byte[] archivoBytes = Base64.getDecoder().decode(fileDTO.getFileContentBase64());
-
-		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-		String fileNameUniq = "file-" + timestamp.getTime() + "." + fileDTO.getFileFormat();
-		String filePath = fileDTO.getFilePath() + fileNameUniq;
-
-		File file = new File(filePath);
+	public static String createFile(byte[] archivoBytes, FileDTO fileDTO) throws IOException {
+		String fileNameUniq = getFileNameUniq(fileDTO);
+		File file = new File(fileDTO.getFilePath() + fileNameUniq);
 
 		try (OutputStream out = new FileOutputStream(file);) {
 			out.write(archivoBytes);
 			return fileNameUniq;
 		}
+	}
+
+	public static String createFileFromBase64(FileDTO fileDTO) throws IOException {
+		byte[] archivoBytes = Base64.getDecoder().decode(fileDTO.getFileContentBase64());
+		String fileNameUniq = getFileNameUniq(fileDTO);
+		File file = new File(fileDTO.getFilePath() + fileNameUniq);
+
+		try (OutputStream out = new FileOutputStream(file);) {
+			out.write(archivoBytes);
+			return fileNameUniq;
+		}
+	}
+
+	public static String getFileNameUniq(FileDTO fileDTO) {
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+		String fileNameUniq = "file-" + timestamp.getTime() + "." + fileDTO.getFileFormat();
+		return fileNameUniq;
 	}
 }
